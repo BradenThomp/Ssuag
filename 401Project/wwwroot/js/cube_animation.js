84c9b230865abca1a969cfc78ce92ef339dd55f1
Braden Thompson <bradent@outlook.com>
2020-02-12 01:09:35 -0700

    Added spinning cube on home page

---
+++ 401Project/wwwroot/js/cube_animation.js
@@ -0,0 +1,50 @@
+﻿let scene, camera, renderer, cube;
+
+function init() {
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / (window.innerHeight * 0.65),
+        0.1,
+        1000
+    );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+
+    renderer.setSize(window.innerWidth, window.innerHeight * 0.65);
+    renderer.setClearColor(0x000000, 0);
+
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(3, 3, 3);
+    const texture = new THREE.TextureLoader().load('../img/logo.jpg');
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+}
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / (window.innerHeight * 0.65);
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight * 0.65);
+}
+
+init();
+
+window.addEventListener('resize', onWindowResize, false);
+
+
+animate();
