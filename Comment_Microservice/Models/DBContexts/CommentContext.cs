1569dc6f603793f47937063005736bfa73daa2c3
Nightingoul <edwardgu0228@outlook.com>
2020-03-08 15:12:00 -0600

    Adding CommentContext

---
+++ Comment_Microservice/Models/DBContexts/CommentContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comment_Microservice.DBContexts
+{
+    public class CommentContext : DbContext
+    {
+    }
+}
